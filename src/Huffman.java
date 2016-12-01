d0942e24d79bf38160a3364c40aabc1be6a5d63f
Zach Litzinger <litzi005@morris.umn.edu>
2016-12-01 15:50:35 -0600

    created Node class and encoder class

--- src/Huffman.java
+++ src/Huffman.java
@@ -1,10 +1,46 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 
 public class Huffman {
 	
 	public static void main(String[] args){
+		ArrayList<String> input = new ArrayList<String>();
 		for (String arg : args){
-			System.out.println(arg);
+			input.add(arg);
 		}
+		//make our nodes	
+		ArrayList<Node> nodes = createNodes(input);
+		System.out.println(nodes.toString());
+		//process our nodes to create tree
+		Node tree = constructTree(nodes);
+		System.out.println(tree.toString());
+		makeCodes(tree);
 		
+	}
+
+	private static void makeCodes(Node tree) {
+		//need to iterate the whole tree
+		
+	}
+
+	private static Node constructTree(ArrayList<Node> nodes) {
+		//sort in reverse order to start at beginning
+		Collections.sort(nodes);
+		//initialize tree
+		Node tree = new Node(nodes.remove(0), nodes.remove(0));
+		for (Node node : nodes){
+			tree = Node.addtoTree(tree, node);
+		}
+		return tree;
+	}
+
+	private static ArrayList<Node> createNodes(ArrayList<String> input) {
+		ArrayList<Node> nodes = new ArrayList<Node>();
+		for (int i = 0; i < input.size(); i = i + 2){
+			Node tempNode = new Node(input.get(i), Integer.parseInt(input.get(i+1)));
+			nodes.add(tempNode);
+		}
+		return nodes;
 	}
 }
---
+++ src/Node.java
@@ -0,0 +1,80 @@
+
+public class Node implements Comparable<Node> {
+	private boolean isLetter;
+	private String letter;
+	private int value;
+	private Node leftChild;
+	private Node rightChild;
+	
+	//This is the constructor for the type of node that have letters and values
+	public Node (String letter, int value){
+		this.isLetter = true;
+		this.letter = letter;
+		this.value = value;
+		this.leftChild = null;
+		this.rightChild = null;
+	}
+	
+	//This is the constructor for the type of node that initializes our tree (and is the head of that tree)
+	public Node (Node leftChild, Node rightChild) {
+		this.isLetter = false;
+		this.value = leftChild.value + rightChild.value;
+		this.leftChild = leftChild;
+		this.rightChild = rightChild;
+	}
+	
+	public Node getLeftChild(){
+		return this.leftChild;
+	}
+	
+	public Node getRightChild(){
+		return this.rightChild;
+	}
+	
+	public static Node addtoTree(Node tree, Node node) {
+		if (tree.value > node.value){
+			return new Node(tree, node);
+		} else {
+			return new Node(node, tree);
+		}
+	}
+	
+	public String getLetter(){
+		if (isLetter) {
+			return this.letter;
+		} else {
+			throw new IndexOutOfBoundsException("You can't get a letter if there is no letter");
+		}
+	}
+	
+	public boolean isLetter(){
+		return this.isLetter;
+	}
+	
+	public int getValue(){
+		return this.value;
+	}
+	
+	public void setValue(int value){
+		if (!isLetter){
+			this.value = value;
+		} else {
+			throw new IndexOutOfBoundsException("You can't set the value if it has a letter");
+		}
+	}
+	
+	public String toString(){
+		if (isLetter){
+			return "{" + letter + ", " + value + "}";
+		} else {
+			return "{value: " + value + "; leftChild: " + leftChild.toString() + "; rightChild: " + rightChild.toString() +"}";
+		}
+		
+	}
+	
+	public int compareTo(Node other){
+		int compare = this.value - other.value;
+	    return compare;
+	}
+
+}
